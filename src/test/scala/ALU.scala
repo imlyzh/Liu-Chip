fd5febff53d76b43ff2c4c32612e71bdd6d964fc
imlyzh <enterindex@gmail.com>
2023-02-04 06:21:22 +0800

    deleted editor cache file

--- src/test/scala/ALU.scala
+++ src/test/scala/ALU.scala
@@ -5,13 +5,15 @@ import chiseltest._
 import org.scalatest.freespec.AnyFreeSpec
 import chisel3.experimental.BundleLiterals._
 
-import ALU._
 import ArchConfig._
+import ALU._
+import RegFile._
 
 
 class ALUTest extends AnyFreeSpec with ChiselScalatestTester {
   // test class body here
   test (new ALU(VLiwXArch)) { alu =>
-    
+    var regfile = new RegFile(VLiwXArch)
+    alu.io.reg <> regfile.io
   }
 }
